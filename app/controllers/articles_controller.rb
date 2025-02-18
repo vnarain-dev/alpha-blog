1a65974fc515c1b415536a11160d2f8170438ac9
Vimal Aiyappath Narain <vnarain.dev@gmail.com>
2025-02-18 10:08:17 -0800

    Add update/edit options to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     #byebug
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -22,6 +26,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
